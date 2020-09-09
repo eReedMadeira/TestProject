73aa67264ad96c76621f26c8b0da6abd1c82867d
Eliza Reed <ereed@madeira.org>
2020-09-09 12:13:33 -0400

    look at all those chickens

---
+++ src/madeira/cs/Main.java
@@ -0,0 +1,8 @@
+package madeira.cs;
+
+public class Main {
+
+    public static void main(String[] args) {
+System.out.println ("Please disregard this project. It only exists so that I can test things on it.");
+    }
+}
